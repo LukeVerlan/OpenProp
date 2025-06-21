10e6b9d40da20c35a98c4b7ddb3c622ba16cd448
LukeVerlan <verlangieriluke@gmail.com>
2025-06-21 13:07:22 -0700

    test

--- ImpulseCalculator.py
+++ ImpulseCalculator.py
@@ -3,6 +3,3 @@
 # This is a comprehensive approximate impulse calculator to reach a desired rocket apogee
 # This object takes in an openRocket .ork file and a desired apogee, spits out a ballpark
 # estimate of the amount of impulse required to get to that height 
-
-def main():
-  print("Hello Rocket ;DDD")
