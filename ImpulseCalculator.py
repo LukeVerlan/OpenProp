634f0079fc7f5bd4b729cad30f6cf37137774317
LukeVerlan <verlangieriluke@gmail.com>
2025-06-21 11:19:26 -0700

    Added Function header

---
+++ ImpulseCalculator.py
@@ -0,0 +1,5 @@
+# IMPULSE CALCULATOR
+#
+# This is a comprehensive approximate impulse calculator to reach a desired rocket apogee
+# This object takes in an openRocket .ork file and a desired apogee, spits out a ballpark
+# estimate of the amount of impulse required to get to that height 
