ac75aca6749ce05ef2407ef06b43149aca713599
mikhail <mikhail.korobkov11@gmail.com>
2025-06-21 12:37:14 -0700

    Added a cooler print

--- ImpulseCalculator.py
+++ ImpulseCalculator.py
@@ -3,3 +3,5 @@
 # This is a comprehensive approximate impulse calculator to reach a desired rocket apogee
 # This object takes in an openRocket .ork file and a desired apogee, spits out a ballpark
 # estimate of the amount of impulse required to get to that height 
+
+print("Mikhail is here")
