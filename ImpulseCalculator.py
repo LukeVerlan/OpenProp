87d2ec20767c5e8940ad662c74051bbf993a4ce8
LukeVerlan <verlangieriluke@gmail.com>
2025-06-22 18:15:03 -0700

    Structured ImpulseCalculator as a class

--- ImpulseCalculator.py
+++ ImpulseCalculator.py
@@ -2,4 +2,9 @@
 #
 # This is a comprehensive approximate impulse calculator to reach a desired rocket apogee
 # This object takes in an openRocket .ork file and a desired apogee, spits out a ballpark
-# estimate of the amount of impulse required to get to that height 
+# estimate of the amount of impulse required to get to that height
+
+class ImpulseCalculator:
+
+  def __init__():
+    self.gravity = 9.80665
